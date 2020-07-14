1ed6a8f7adb6ce98b2e51269559e842cb266afb9
benjamam <benjamam@miamioh.edu>
2020-07-13 21:06:43 -0400

    initial changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Link to="/about/">About</Link>
+      <h1>Adam was here</h1>
+      <p>This is pretty cool</p>
+    </div>
+  )
 }
